f17e548137c6d3bc9f0f490c9fe180b53854ed7c
janiceiv <janice.ivena@ti.ukdw.ac.id>
2026-05-08 13:01:37 +0700

    add product service

--- product-service/src/main/java/com/berijalan/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/berijalan/product_service/controller/ProductController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/product")
+@RequestMapping("/product")
 public class ProductController {
 
     private final ProductService productService;
