f61cb209458034da8205a43a2497967d748af2f0
secondio10 <168173824+secondioprawiro@users.noreply.github.com>
2026-05-12 19:35:06 +0700

    feat(be-merchant): add Redis caching and GET /merchant/me endpoint

--- be-merchant/merchant-service/src/main/java/com/berijalan/merchant_service/service/impl/ProductServiceImpl.java
+++ be-merchant/merchant-service/src/main/java/com/berijalan/merchant_service/service/impl/ProductServiceImpl.java
@@ -6,6 +6,7 @@ import com.berijalan.merchant_service.dto.response.ResProductDto;
 import com.berijalan.merchant_service.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -16,11 +17,13 @@ public class ProductServiceImpl implements ProductService {
     private final ProductFeignClient productClient;
 
     @Override
+    @Cacheable(value = "products-list", key = "#type ?: 'ALL'")
     public BaseResponse<?> getAllProduct(String type) {
         return productClient.getAllProduct(type);
     }
 
     @Override
+    @Cacheable(value = "products", key = "#productId")
     public BaseResponse<ResProductDto> getProductById(String productId) {
         return productClient.getProductById(productId);
     }
